4e17e41f71598a753b84fc61e75c043372d386f7
Levison-Jr <levisonjr21@gmail.com>
2022-12-13 19:59:15 -0300

    atualização na tentativa de exercicio

--- exercicios/beecrowd/1138v4/main.go
+++ exercicios/beecrowd/1138v4/main.go
@@ -1,15 +1,15 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"strconv"
-	"strings"
 )
 
 func main() {
 	var (
-		A, B int
-		cont []int = make([]int, 10)
+		A, B int64
+		cont []int64 = make([]int64, 10)
 	)
 	for {
 		fmt.Scanln(&A, &B)
@@ -17,22 +17,13 @@ func main() {
 			break
 		}
 		for i := A; i <= B; i++ {
-			s := strconv.FormatInt(int64(i), 10)
-			cont[0] += strings.Count(s, "0")
-			cont[1] += strings.Count(s, "1")
-			cont[2] += strings.Count(s, "2")
-			cont[3] += strings.Count(s, "3")
-			cont[4] += strings.Count(s, "4")
-			cont[5] += strings.Count(s, "5")
-			cont[6] += strings.Count(s, "6")
-			cont[7] += strings.Count(s, "7")
-			cont[8] += strings.Count(s, "8")
-			cont[9] += strings.Count(s, "9")
+			s := strconv.FormatInt(i, 10)
+			cont[0] += int64(bytes.Count([]byte(s), []byte("0")))
 		}
 		for _, v := range cont {
 			fmt.Print(v, " ")
 		}
 		fmt.Println()
-		cont = make([]int, 10)
+		cont = make([]int64, 10)
 	}
 }
